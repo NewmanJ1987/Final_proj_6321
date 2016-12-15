b312c1abb6b5fd324ef65c47a08ea3e1b9e8aeae
Newman Thurairatnam <newman.joyal@gmail.com>
2016-12-14 23:55:20 -0500

    Modifications to csv parser

--- test_mongo/csv_parser.py
+++ test_mongo/csv_parser.py
@@ -64,14 +64,41 @@ genres = ["Musical", "IMAX", "Documentary", "Sci-Fi", "Mystery", "Horror", "Come
           "Adventure", "Fantasy", "Romance", "Crime", "(no genres listed)", "Film-Noir", "Western", "Action", "Drama",
           "Thriller"]
 
-ratings = list(db.ratings.find())
-inputs = [rating.get("genre_info") for rating in ratings]
-outputs = [rating.get("recommend") for rating in ratings]
+# ratings = list(db.ratings.find())
+# inputs = [rating.get("genre_info") for rating in ratings]
+# outputs = [rating.get("recommend") for rating in ratings]
+#
+# print(len(inputs))
+# print(len(outputs))
+#
+# inputs_train, inputs_test, outputs_train, outputs_test = train_test_split(inputs, outputs, test_size=0.1)
+#
+# classifier = GaussianNB()
+# classifier.fit(inputs_train, outputs_train)
+#
+# misclassification = 0
+# total = 0
+# for index in range(len(inputs_test)):
+#     prediction = classifier.predict(inputs_test[index])
+#     total += 1
+#     if prediction != outputs_test[index]:
+#         misclassification += 1
+#
+# # 24 percent misclassification with bayes just using genre.
+# print "Number of misclassification [", misclassification, "] percentage [", misclassification * 1.0 / total, "] "
+
+
+
+# Result for 651 65191
+
+ratings_651 = list(db.ratings.find({"user_id": "651"}))
+inputs = [rating.get("genre_info") for rating in ratings_651]
+outputs = [rating.get("recommend") for rating in ratings_651]
 
 print(len(inputs))
 print(len(outputs))
 
-inputs_train, inputs_test, outputs_train, outputs_test = train_test_split(inputs, outputs, test_size=0.1)
+inputs_train, inputs_test, outputs_train, outputs_test = train_test_split(inputs, outputs, test_size=0.05)
 
 classifier = GaussianNB()
 classifier.fit(inputs_train, outputs_train)
@@ -84,6 +111,5 @@ for index in range(len(inputs_test)):
     if prediction != outputs_test[index]:
         misclassification += 1
 
-# 24 percent misclassification with bayes just using genre.
-print "Number of misclassification [", misclassification, "] percentage [", misclassification * 1.0 / total, "] "
-
+print "Result for user id 651."
+print "Number of misclassification [", misclassification, "] samples in test [", total, "] percentage [", round(misclassification * 1.0 / total,3), "] "
